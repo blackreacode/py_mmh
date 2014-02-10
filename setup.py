1d1d79ed6807207d50861e7bdb4089f16aa691fb
yong.li <liyong19861014@gmail.com>
2014-02-10 11:30:40 +0800

    add impl for murmurhash2

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#! coding: utf-8
+
+import sys 
+
+try:
+        from setuptools import setup, Extension
+except ImportError:
+        from distutils.core import setup, Extension
+
+extra_compile_args = ['-g', '-fPIC', '-Wall', '-O2']
+
+setup(
+	name = 'mmh',
+	version = '1.0',
+	maintainer = 'Michael Lee',
+	maintainer_email = 'liyong19861014@gmail.com',
+	url = 'https://github.com/airhuman/py_mmh.git',
+	description = 'Python bindings for Google Murmurhash2 hash algorithm',
+	ext_modules = [
+		Extension('mmh', 
+			sources = [
+				'mmh.cc'
+			],
+			extra_compile_args = extra_compile_args
+		)
+	]
+)
